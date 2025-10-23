4469be462ffa7d4f6882ae88c6be23b7a364461a
NicholasClooney <137233165+NicholasClooney@users.noreply.github.com>
2025-10-23 10:15:15 +0100

    Implement the `PLAN.md`

---
+++ .eleventy.js
@@ -0,0 +1,17 @@
+const yaml = require("js-yaml");
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addDataExtension("yaml", (contents) => yaml.load(contents));
+  eleventyConfig.addDataExtension("yml", (contents) => yaml.load(contents));
+  eleventyConfig.addPassthroughCopy("css");
+
+  return {
+    dir: {
+      input: ".",
+      includes: "_includes",
+      data: "_data",
+      output: "_site",
+    },
+    templateFormats: ["njk", "md", "html"],
+  };
+};
